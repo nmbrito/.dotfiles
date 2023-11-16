aded65dd00fef3b493b98471f44c7079a2e947e8
operador <brito.n@outlook.pt>
2023-11-16 18:43:01 +0000

    Updated config/zsh/zshrc-alias.zsh.

--- config/zsh/zshrc-alias.zsh
+++ config/zsh/zshrc-alias.zsh
@@ -2,7 +2,8 @@
 alias lsd="ls -slaF --color=auto"                                       # Lists with -sla (with hidden folders and more)
 alias sld="ls -sl --color=auto"                                         # Lists with -sl (no hidden folders)
 alias cd.="cd ${path_dotrepo}"                                          # Changes to .dotfiles directory
-alias catalias="cat ${path_dotrepo}/config/zsh/zshrc-alias.zsh | less"  # Cats my alias
+alias cat="bat"                                                         # Subs cat for bat
+alias catalias="bat ${path_dotrepo}/config/zsh/zshrc-alias.zsh | less"  # Cats my alias
 alias reload=". ${HOME}/.zshrc"                                         # reloads zshrc config
 alias vis="vim -S ${path_vimsessions}/current_session.vim"              # Loads vim current session
 function shcheck() { shellcheck "$1" -x --color | less -r }             # Shellcheck with color and less active
