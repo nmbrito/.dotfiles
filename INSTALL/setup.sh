ca7631b593ff275495c5b11c2780f99bac24f198
nmbrito <brito.n@outlook.pt>
2024-05-07 16:07:37 +0100

    Updated INSTALL/setup.sh.

--- INSTALL/setup.sh
+++ INSTALL/setup.sh
@@ -22,6 +22,7 @@
         . /etc/os-release
     #elif [ -n "$(sw_vers)" ] ; then
     elif [ ! command -v sw_vers >/dev/null 2>&1 ] ; then
+        echo "crap"
         ID="$(sw_vers -productName)"
     fi
 
