e1aa6cd74edd8c5d60faad2180f4b090be7079b6
nmbrito <brito.n@outlook.pt>
2024-06-09 15:56:41 +0100

    Updated config/zsh/zshrc-alias.zsh.

--- config/zsh/zshrc-alias.zsh
+++ config/zsh/zshrc-alias.zsh
@@ -16,34 +16,6 @@ function shcheck() { shellcheck "$1" -x --color | less -r }                     
 #alias lsd="ls -slaF --color=auto"                                       # Lists with -sla (with hidden folders and more)
 #alias sld="ls -sl --color=auto"                                         # Lists with -sl (no hidden folders)
 
-# Backup Plasma Settings        {{{1
-function backupkde()
-{
-    # Files in .config
-    cp -v "${HOME}/.config/kglobalshortcutsrc"                      "${path_dotrepo}/config/plasma/"    # Keyboard shortcuts
-    cp -v "${HOME}/.config/kwinrc"                                  "${path_dotrepo}/config/plasma/"    # Kwin effects
-    cp -v "${HOME}/.config/plasma-localerc"                         "${path_dotrepo}/config/plasma/"    # All locale PT
-    cp -v "${HOME}/.config/plasma-org.kde.plasma.desktop-appletsrc" "${path_dotrepo}/config/plasma/"    # Applets in panels
-    cp -v "${HOME}/.config/plasmashellrc"                           "${path_dotrepo}/config/plasma/"    #
-    cp -v "${HOME}/.config/powermanagementprofilesrc"               "${path_dotrepo}/config/plasma/"    # Energy profiles
-    cp -v "${HOME}/.config/strawberry/strawberry.conf"              "${path_dotrepo}/config/strawberry" # Strawberry
-
-    # Files in .local
-    cp -v "${HOME}/.local/share/konsole/mytik.profile"  "${path_dotrepo}/config_local/share/konsole/mytik.profile"  # Konsole profile
-
-    # Folders
-    cp -rv "${HOME}/.config/kdedefaults"                "${path_dotrepo}/config/plasma/"
-
-    # Themes
-    tar czvf ${path_dotrepo}/plasma_globalthemes.tar.gz \
-        ${HOME}/.local/share/plasma \
-        ${HOME}/.local/share/color-schemes \
-        ${HOME}/.local/share/icons \
-        --wildcards ${HOME}/.local/share/konsole/*.colorscheme \
-        ${HOME}/.local/share/kwin \
-        ${HOME}/.local/share/wallpapers \
-}
-
 # GIT                           {{{1
 alias gitsubup="git submodule update --remote"  # Updates git submodules
 alias gitsubstat="git submodule status"         # Updates git submodules
