98039b4342048f74387587ae4711a004b1eb621f
nmbrito <brito.n@outlook.pt>
2024-06-21 09:26:23 +0100

    Updated INSTALL/setup.sh.

--- INSTALL/setup.sh
+++ INSTALL/setup.sh
@@ -4,44 +4,33 @@
 # Description: semi-automatic personal installer
 
 # Init systems
-# -----------------
-#   Run script from any directory
-    dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+# ------------
 
-#   Vars
-    dir_dotroot="$(git rev-parse --show-toplevel)"
-    dir_cache="${HOME}/.cache"
-    url_nerdfonts="https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest"
+#   Variables
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)    # Run script from any directory
 
-    message_longwarn="###########################################"
-    message_longdash="----------------------"
-    message_execroot="# The following commands will run as ROOT #"
+dir_dotroot="$(git rev-parse --show-toplevel)"      # Define .dotfiles directory
+dir_cache=""${HOME}"/.cache"                        # Define .cache directory
 
-#   Source components
-    if [ -h "/etc/os-release" ] ; then
-        . /etc/os-release
-    #elif [ -n "$(sw_vers)" ] ; then
-    elif [ ! command -v sw_vers >/dev/null 2>&1 ] ; then
-        ID="$(sw_vers -productName)"
-    fi
+url_nerdfonts="https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest"
 
-    if [ ! -f "${dir}"/functions.sh ] && [ ! -f "${dir}"/packages.sh ] ; then
-        printf '%s\n' "Missing install components. Aborting."
-        exit 0
-    fi
+message_longwarn="###########################################"
+message_longdash="----------------------"
+message_execroot="# The following commands will run as ROOT #"
 
-#   Source file containing functions.
-    . "${dir}"/functions.sh
+if   [ -L /etc/os-release                  ]; then . /etc/os-release;                   # Linux distribuition
+elif [ command -v sw_vers > /dev/null 2>&1 ]; then ID="$(sw_vers -productName)"; fi     # macOS
+if   [ -n "${WT_SESSION}"                  ]; then wslsession=1; else wslsession=0; fi  # WSL session
 
-#   Defines the package manager and software especific to the running distribution.
-    functionDefineDistro 
-
-#   Define current host
-    functionDefineHost
-
-#   Source file containing software list.
-#   This needs to be here so it can load after the /func_def_distro/ function.
-    . "${dir}"/packages.sh
+if [ ! -f "${dir}"/functions.sh ] && [ ! -f "${dir}"/packages.sh ] ; then
+    printf '%s\n' "Missing install components. Aborting."
+    exit 0
+elif
+    . "${dir}"/functions.sh # Source file containing functions.
+    functionDefineDistro    # Defines the package manager and software especific to the running distribution.
+    functionDefineHost      # Define current host
+    . "${dir}"/packages.sh  # Sourced after functions.sh
+fi
 
 # Main
 # ----
@@ -49,9 +38,8 @@ printf '%s\n'   "" \
                 "Starting process" \
                 "${message_longdash}" \
                 "" \
-                "Current distro: ${ID}"
+                "Current distro: "${ID}""
 
-# Call function
 while :
 do
     functionBuildMenu
@@ -62,9 +50,9 @@ do
 
     case "${option_picked}" in
         1)
-            functionInstallFixes
             functionInstallRepositories
             functionInstallPackages
+            functionInstallFixes
             functionInstallFonts
             functionInstallSymlinks
             functionConfigGitGlobals
@@ -72,26 +60,16 @@ do
             functionConfigVimHelptags
             functionConfigShell
             ;;
-        2)
-            functionInstallFixes ;;
-        3)
-            functionInstallRepositories ;;
-        4)
-            functionInstallPackages ;;
-        5)
-            functionInstallFonts ;;
-        6)
-            functionInstallSymlinks ;;
-        7)
-            functionConfigGitGlobals ;;
-        8)
-            functionInstallGitSubmodules ;;
-        9)
-            functionConfigVimHelptags ;;
-        10)
-            functionConfigShell ;;
-        r)
-            functionRebuildGitSubmodules ;;
+        2)  functionInstallRepositories     ;;
+        3)  functionInstallPackages         ;;
+        4)  functionInstallFixes            ;;
+        5)  functionInstallFonts            ;;
+        6)  functionInstallSymlinks         ;;
+        7)  functionConfigGitGlobals        ;;
+        8)  functionInstallGitSubmodules    ;;
+        9)  functionConfigVimHelptags       ;;
+        10) functionConfigShell             ;;
+        r)  functionRebuildGitSubmodules    ;;
         *)
             printf '%s\n'   "Exiting..." \
                             ""
