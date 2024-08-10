eb3cd8ef00d8d4205ec51e5c67ce42945d6d8847
nmbrito <brito.n@outlook.pt>
2024-08-10 20:30:00 +0100

    Updated INSTALL/functions.sh.

--- INSTALL/functions.sh
+++ INSTALL/functions.sh
@@ -252,10 +252,7 @@ functionInstallPackages()
         *)
             case "${XDG_SESSION_DESKTOP}" in
                 "KDE")
-                    (su -c "${pkginst}
-                            ${packages_terminal}
-                            ${packages_kde_basics}
-                            ");
+                    (su -c "${pkginst} ${packages_terminal} ${packages_kde_basics}");
                     ;;
                 *) ;;
             esac
