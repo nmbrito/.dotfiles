6e5f5824e2091b7df7b24d9e1e144c9aad0fe201
nmbrito <brito.n@outlook.pt>
2024-09-12 20:16:03 +0100

    Updated INSTALL/packages.sh.

--- INSTALL/packages.sh
+++ INSTALL/packages.sh
@@ -3,6 +3,26 @@
 # Quickly sort lists with vim :'<,'>sort
 # Removed:
 #   neofetch
+list_server_gui="\
+    xorgxrdp\
+    xrdp"
+
+list_server_cli="\
+    ${sw_fd}\
+    ${sw_fdzshcompletion}\
+    ${sw_fzftmux}\
+    ${sw_fzfzshcompletion}\
+    ${sw_vimdata}\
+    curl\
+    fzf\
+    htop\
+    ripgrep-zsh-completion\
+    ripgrep\
+    tmux\
+    vifm\
+    vim-fzf\
+    vim\
+    zsh"
 
 packages_terminal="\
     ${sw_btop}\
