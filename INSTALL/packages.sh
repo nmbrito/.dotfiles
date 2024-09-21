513388804d1abbaaecd4143e207a81f653d1e829
nmbrito <brito.n@outlook.pt>
2024-09-21 11:34:25 +0100

    Updated INSTALL/packages.sh.

--- INSTALL/packages.sh
+++ INSTALL/packages.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # Quickly sort lists with vim :'<,'>sort
+
+# Packages
+# Servers {{{1
+# Shiny Server {{{2
 packages_shinyserver="\
     apache2\
     build-essential\
@@ -30,8 +34,10 @@ packages_shinyserver="\
     r-cran-mass\
     r-cran-nlme\
     r-cran-sp\
-    r-cran-spatial"\
+    r-cran-spatial"
+# 2}}}
 
+# R Modules {{{2
 packages_rmodules="\
     'DBI'\
     'DT'\
@@ -99,17 +105,21 @@ packages_rmodules="\
     'tidyverse'\
     'toastui'\
     'waiter'"
+# 2}}}
 
+# Server GUI X11 {{{2
 list_server_gui="\
     xorgxrdp\
     xrdp"
+# 2}}}
 
+# Server CLI {{{2
 list_server_cli="\
-    ${sw_fd}\
-    ${sw_fdzshcompletion}\
-    ${sw_fzftmux}\
-    ${sw_fzfzshcompletion}\
-    ${sw_vimdata}\
+    ${software_fd}\
+    ${software_fdzshcompletion}\
+    ${software_fzftmux}\
+    ${software_fzfzshcompletion}\
+    ${software_vimdata}\
     curl\
     fzf\
     htop\
@@ -120,16 +130,20 @@ list_server_cli="\
     vim-fzf\
     vim\
     zsh"
+# 2}}}
+# 1}}}
 
+# Personal {{{1
+# Terminal {{{2
 packages_terminal="\
-    ${sw_btop}\
-    ${sw_fd}\
-    ${sw_fdzshcompletion}\
-    ${sw_fzftmux}\
-    ${sw_fzfzshcompletion}\
-    ${sw_rgzshcompletion}\
-    ${sw_vifmcolors}\
-    ${sw_vimdata}\
+    ${software_btop}\
+    ${software_fd}\
+    ${software_fdzshcompletion}\
+    ${software_fzftmux}\
+    ${software_fzfzshcompletion}\
+    ${software_rgzshcompletion}\
+    ${software_vifmcolors}\
+    ${software_vimdata}\
     bat\
     eza\
     curl\
@@ -143,23 +157,31 @@ packages_terminal="\
     vifm\
     vim\
     zsh"
+# 2}}}
 
+# Development {{{2
 packages_dev="\
-    ${sw_shellcheck}\
+    ${software_shellcheck}\
     gcc\
     gdb\
     make\
     valgrind"
+# 2}}}
 
+# WSL Pattern for openSUSE {{{2
 packages_wsl_pattern="\
     wsl_gui\
     wsl_base\
     wsl_systemd"
+# 2}}}
 
+# KDE Basics {{{2
 packages_kde_basics="\
     keepassxc\
     myspell-pt_PT"
+# 2}}}
 
+# KDE Personal {{{2
 packages_kde_personal="\
     blender\
     code\
@@ -178,7 +200,16 @@ packages_kde_personal="\
     telegram-desktop\
     virt-manager\
     vlc"
+# 2}}}
 
+# Hyprland {{{2
+packages_hyprland="\
+    hyprland\
+    hyprland-devel\
+    kitty"
+# 2}}}
+
+# ThinkPad x230 {{{2
 packages_x230="\
     fprintd\
     fprintd-pam\
@@ -187,7 +218,9 @@ packages_x230="\
     NetworkManager-fortisslvpn\
     openfortivpn\
     plasma-nm5-fortisslvpn"
+# 2}}}
 
+# iPadOS and iOS iSH Terminal {{{2
 packages_ish="\
     build-base\
     gcc\
@@ -199,7 +232,10 @@ packages_ish="\
     openssh\
     vifm-colors --force-overwrite\
     zsh-vcs"
+# 2}}}
+# 1}}}
 
+# Fonts {{{1
 packages_fonts="\
     CascadiaCode.tar.xz\
     FiraCode.tar.xz\
@@ -207,66 +243,5 @@ packages_fonts="\
     Lilex.tar.xz\
     JetBrainsMono.tar.xz\
     Monoid.tar.xz"
+# 1}}}
 
-backup_kde="\
-    [ -d "${HOME}/.local/share/aurorae" ]               && cp -rv "${nmb_dotrepo}/kde_backup/"\
-    [ -d "${HOME}/.local/share/color-schemes" ]         && cp -rv "${nmb_dotrepo}/kde_backup/"\
-    [ -d "${HOME}/.local/share/icons" ]                 && cp -rv "${nmb_dotrepo}/kde_backup/"\
-    [ -d "${HOME}/.local/share/plasma/desktoptheme" ]   && cp -rv "${nmb_dotrepo}/kde_backup/"\
-    [ -d "${HOME}/.local/share/plasma/look-and-feel" ]  && cp -rv "${nmb_dotrepo}/kde_backup/"\
-    [ -d "${HOME}/.local/share/wallpapers" ]            && cp -rv "${nmb_dotrepo}/kde_backup/"\
-    [ -d "${HOME}/.icons" ]                             && cp -rv "${nmb_dotrepo}/kde_backup/"\
-    "
-symlinks_base="\
-    [ ! -d "${HOME}"/.config/mc ]       && mkdir "${HOME}"/.config/mc\
-
-    [ -d "${HOME}"/.vim ]               && rm -rf "${HOME}"/.vim\
-    [ -d "${HOME}"/.config/vifm ]       && rm -rf "${HOME}"/.config/vifm\
-    [ -d "${HOME}"/.config/neofetch ]   && rm -rf "${HOME}"/.config/neofetch\
-    [ -d "${HOME}"/.config/tmux ]       && rm -rf "${HOME}"/.config/tmux\
-
-    ln -vsf "${dir_dotroot}"/config/zsh/zshrc   "${HOME}"/.zshrc\
-    ln -vsf "${dir_dotroot}"/config/vim/vimrc   "${HOME}"/.vimrc\
-
-    ln -vsf "${dir_dotroot}"/config/mc          "${HOME}"/.config/mc\
-    ln -vsf "${dir_dotroot}"/config/vim         "${HOME}"/.vim\
-    ln -vsf "${dir_dotroot}"/config/vifm        "${HOME}"/.config/vifm\
-    ln -vsf "${dir_dotroot}"/config/neofetch    "${HOME}"/.config/neofetch\
-    ln -vsf "${dir_dotroot}"/config/tmux        "${HOME}"/.config/tmux\
-    ln -vsf "${dir_dotroot}"/config/fd          "${HOME}"/.config/fd"
-
-restore_kde="\
-
-
-    "
-#    # If KDE is detected as current session.
-#    if [ "${XDG_SESSION_DESKTOP}" = "KDE" ]
-#    then
-#        for kde_configs in $kde_config_list
-#        do
-#            cp -v "${dir_dotroot}"/config/${kde_configs}    "${HOME}"/.config/
-#        done
-#        # KDE configuration files must be copied.
-#        # KDE is unable to save settings with symlinks.
-#        [ -d "${HOME}"/.config/kdedefaults ] && rm -rf "${HOME}"/.config/kdedefaults
-#
-#        cp -v "${dir_dotroot}"/config/plasma/kglobalshortcutsrc                     	"${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/plasma/kwinrc                                 	"${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/plasma/plasma-localerc                            "${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/plasma/plasma-org.kde.plasma.desktop-appletsrc	"${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/plasma/plasmashellrc                          	"${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/plasma/powermanagementprofilesrc                  "${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/strawberry/strawberry.conf                        "${HOME}"/.config/strawberry
-#
-#        cp -v "${dir_dotroot}"/config_local/share/konsole/mytik.profile             	"${HOME}"/.local/share/konsole/
-#        cp -v "${dir_dotroot}"/config_local/share/konsole/Edna.colorscheme             	"${HOME}"/.local/share/konsole/
-#
-#        cp -v "${dir_dotroot}"/config/plasma/new/konsolerc                              "${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/plasma/new/dolphinrc                              "${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/plasma/new/kdeglobals                             "${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/plasma/new/kmixrc                                 "${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/plasma/new/ksplashrc                              "${HOME}"/.config/
-#        cp -v "${dir_dotroot}"/config/plasma/new/ktimezonedrc                           "${HOME}"/.config/
-#
-#        cp -rv "${dir_dotroot}"/config/plasma/kdedefaults                               "${HOME}"/.config/
-#    fi
