1bfbe2b516e42aae16ee3c7f4a671adb548c5745
nmbrito <brito.n@outlook.pt>
2024-09-26 15:18:49 +0100

    Updated INSTALL/server/shiny-server-20240926.sh.

--- INSTALL/server/shiny-server-20240926.sh
+++ INSTALL/server/shiny-server-20240926.sh
@@ -41,81 +41,86 @@ fi
 
 [ ! -d "${path_cache}" ] && mkdir "${path_cache}"
 
-    stty -echo
-        printf '%s\n' "Switching to root user. Insert password: "
-        read root_pw
-    stty echo
-    printf '%s\n' ""
+printf '%s\n'   "                                                                " \
+                "|--------------------------------------------------------------|" \
+                "| Information:                                                 |" \
+                "|--------------------------------------------------------------|" \
+                "|  host: ${current_host}                                        " \
+                "|  distribuition: "${ID}"                                       " \
+                "|  package manager: "${pkg_manager}"                            " \
+                "|  current shell: "${SHELL}"                                    " \
+                "|  pwd: $(pwd)                                                  " \
+                "|  repository root: "${path_dotroot}"                           " \
+                "|  cache directory: "${path_cache}"                             " \
+                "|                                                               " \
+                "|  Script path: "${path_script}"                                " \
+                "|  Configurations path: "${path_config_files}"                  " \
+                "|  Utilities path: "${path_utilities}"                          " \
+                "|  Cache path: "${path_cache}"                                  " \
+                "|  Shiny server /etc path: "${path_shinyserver_etc}"            " \
+                "|  Shiny server /log path: "${path_shinyserver_log}"            " \
+                "|  Shiny server /bin path: "${path_shinyserver_bin}"            " \
+                "|  Shiny server /srv path: "${path_shinyserver_srv}"            " \
+                "|  Shiny server URL download: "${urldownload_shinyserver}"      " \
+                "|  Shiny server package downloaded name: "${urlpkg_shinyserver}"" \
+                "|  Apache2 site-available path: "${path_apache_sites}"          " \
+                "|  Shiny user: "${user_shiny}"                                  " \
+                "|  Shiny group: "${group_shiny}"                                " \
+                "|--------------------------------------------------------------|" \
+                "| Shiny Server Installer                                       |" \
+                "|--------------------------------------------------------------|" \
+                "                                                                "
 
-# R Repository
-    printf "%s" "${root_pw}" | su -c "
+    printf '%s\n' "The next commands will run as ROOT"
+    su -c "
         printf '%s\n' "Adding R repository." ;
         gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7' ;
         gpg --armor --export '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7' | sudo tee /etc/apt/trusted.gpg.d/cran_debian_key.asc ;
         add-apt-repository 'deb http://cloud.r-project.org/bin/linux/debian bookworm-cran40/ bookworm main' ;
-        "
 
-# Update repository list
-    printf "%s" "${root_pw}" | su -c "
         printf '%s\n' "Performing update on repository sources list." ;
         apt update ;
-        "
 
-# ShinyServer dependencies
-    printf "%s" "${root_pw}" | su -c "
         printf '%s\n' "Installing shiny server dependencies." ;
         ${pkg_installcommmand} ${packages_shinyserver} ;
-        "
 
-# R modules
-    printf "%s" "${root_pw}" | su -c "
         printf '%s\n' "Installing R modules." ;
         R -e \"install.packages(c(${packages_rmodules}), repos='https://cran.rstudio.com/')\" ;
-        "
 
-# Download and install ShinyServer
-    printf "%s" "${root_pw}" | su -c "
         printf '%s\n' "Downloading and installing ShinyServer" ;
         curl -L $(curl -s "${urldownload_shinyserver}" | grep -Eo "wget .+?shiny-server-[0-9\.]+-amd64.deb" ) --output "${path_cache}" ;
         gdebi ${path_cache}/${urlpkg_shinyserver} ;
-        "
-        #Alternative: sudo su - -c "R -e \"install.packages(c(${packages_rmodules}), repos='https://cran.rstudio.com/')\""
 
-# Apache2 reverse proxy modules
-    printf "%s" "${root_pw}" | su -c "
+        printf '%s\n' "Enabling Apache2 modules" ;
         a2dissite 000-default.conf ;
         a2enmod proxy ;
         a2enmod proxy_http ;
         a2enmod proxy_wstunnel ;
         a2enmod rewrite ;
         a2enmod headers ;
+
+        chown -R ${user_shiny}:${group_shiny} ${path_shinyserver_log} ;
+        cp -rv ${file_shinyserver_conf} ${path_shinyserver_etc} ;
+        chown root:root ${path_shinyserver_etc}/${file_shinyserver_conf} ;
+        chmod u=rw,go=r ${path_shinyserver_etc}/${file_shinyserver_conf} ;
+
+        crontab -u shiny -l ;
+        echo '* * * * * find /var/lib/shiny-server/bookmarks/shiny/dashboard-3e1c00a993afd727238cd8a008163d1b/ -type d -ctime +30 -exec rm -fr {} +'
         "
 
-# Configurations and Permissions
+    printf '%s\n' "The next commands will run as ROOT, for each file"
     for $apache_item in $file_apachelist
     do
-        printf "%s" "${root_pw}" | su -c "
-            cp -rv $apache_item ${path_apache_sites}
-            a2ensite $apache_item
+        su -c "
+            printf '%s\n' "Copying ${apache_item} to ${path_apache_sites}" ;
+            cp -rv $apache_item ${path_apache_sites} ;
+            printf '%s\n' "Enabling ${apache_item} site" ;
+            a2ensite $apache_item ;
             "
     done
 
-    printf "%s" "${root_pw}" | su -c "
-        chown -R ${user_shiny}:${group_shiny} ${path_shinyserver_log}
-        cp -rv ${file_shinyserver_conf} ${path_shinyserver_etc}
-        chown root:root ${path_shinyserver_etc}/${file_shinyserver_conf}
-        chmod u=rw,go=r ${path_shinyserver_etc}/${file_shinyserver_conf}
-        "
-
-# Cronjob
-    printf "%s" "${root_pw}" | su -c "
-        crontab -u shiny -l;
-        echo "* * * * * find /var/lib/shiny-server/bookmarks/shiny/dashboard-3e1c00a993afd727238cd8a008163d1b/ -type d -ctime +30 -exec rm -fr {} +"
-        "
-
-# Final
-    printf "%s" "${root_pw}" | su -c "
-        systemctl restart shiny-server.service
-        systemctl restart apache2.service
+    su -c "
+        systemctl restart shiny-server.service;
+        systemctl restart apache2.service;
     "
 
