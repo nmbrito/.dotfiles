0684659459efba4a8b851653e2337d78a217c6a9
nmbrito <brito.n@outlook.pt>
2024-11-01 17:25:11 +0000

    Updated config/colorscheme.sh.

---
+++ config/colorscheme.sh
@@ -0,0 +1,82 @@
+# New colorscheme:
+nmbcolor_black   = #272822
+nmbcolor_brown   = #87875f
+nmbcolor_gray    = #C0C0C0 
+nmbcolor_gray2   = #585858
+nmbcolor_gray3   = #75715e
+nmbcolor_red     = #ff0000
+nmbcolor_green   = #87ff00
+nmbcolor_green2  = #5fd700
+nmbcolor_orange  = #ff5f00
+nmbcolor_yellow  = #d7af00
+nmbcolor_blue    = #00afff
+nmbcolor_blue2   = #66d9ef
+nmbcolor_cyan    = #a1efe4
+nmbcolor_purple  = #ae81ff
+nmbcolor_white   = #f8f8f2
+
+# Original colorscheme:
+nmbcolor_black   = #272822
+nmbcolor_gray    = #75715e
+nmbcolor_red     = #f92672
+nmbcolor_green   = #a6e22e
+nmbcolor_orange  = #fd971f
+nmbcolor_yellow  = #ffd866
+nmbcolor_blue    = #66d9ef
+nmbcolor_cyan    = #a1efe4
+nmbcolor_purple  = #ae81ff
+nmbcolor_white   = #f8f8f2
+
+# Ohmyposh
+nmbcolor_black="#272822"
+nmbcolor_brown="#87875f"
+nmbcolor_gray="#C0C0C0"
+nmbcolor_gray2="#585858"
+nmbcolor_gray3="#75715e"
+nmbcolor_red="#ff0000"
+nmbcolor_green="#87ff00"
+nmbcolor_green2="#5fd700"
+nmbcolor_orange="#ff5f00"
+nmbcolor_yellow="#d7af00"
+nmbcolor_blue="#00afff"
+nmbcolor_blue2="#66d9ef"
+nmbcolor_cyan="#a1efe4"
+nmbcolor_purple="#ae81ff"
+nmbcolor_white="#f8f8f2"
+
+# TMUX
+# Monokai.dark (classic) from Terminal.sexy
+nmbcolor_black="#272822"
+nmbcolor_grey="#75715e"
+nmbcolor_red="#f92672"
+nmbcolor_green="#a6e22e"
+nmbcolor_orange="#fd971f" 
+nmbcolor_yellow="#ffd866"
+nmbcolor_blue="#66d9ef"
+nmbcolor_cyan="#a1efe4"
+nmbcolor_purple="#ae81ff"
+nmbcolor_white="#f8f8f2"
+
+# Monokai new scheme
+nmbcolor_black="#272822"
+nmbcolor_grey="#75715e"
+nmbcolor_red="#ff0000"
+nmbcolor_green="#87ff00"
+nmbcolor_orange="#ff5f00" 
+nmbcolor_yellow="#d7af00"
+nmbcolor_blue="#00afff"
+nmbcolor_cyan="#a1efe4"
+nmbcolor_purple="#ae81ff"
+nmbcolor_white="#f8f8f2"
+
+# Monokai Merged
+nmbcolor_black="#272822"
+nmbcolor_grey="#75715e"
+nmbcolor_red="#ff0000"
+nmbcolor_green="#a6e22e"
+nmbcolor_orange="#ff5f00" 
+nmbcolor_yellow="#ffd866"
+nmbcolor_blue="#00afff"
+nmbcolor_cyan="#a1efe4"
+nmbcolor_purple="#ae81ff"
+nmbcolor_white="#f8f8f2"
