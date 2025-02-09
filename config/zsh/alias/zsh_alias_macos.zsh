f34020766527bb8eed8b8e5b672665877275eaba
nmbrito <brito.n@outlook.pt>
2025-02-09 16:52:43 +0000

    Updated config/zsh/alias/zsh_alias_macos.zsh.

--- config/zsh/alias/zsh_alias_macos.zsh
+++ config/zsh/alias/zsh_alias_macos.zsh
@@ -12,3 +12,15 @@ alias brewup="brew update && brew upgrade"                             # Updates
 #alias b="brew remove"                                                  # Remove package
 #alias b="brew uninstall"                                               # Uninstall package
 #alias b="brew uninstall --cask"                                        # Uninstall gui package
+
+# Function alias
+function burn2usb()
+{
+    hdiutil convert ${1}.iso -format UDRW -o ${1}.dmg
+    diskutil list
+    printf '%s' "Number of the USB Flash Drive: "
+    read -r usbdrivenumber
+    diskutil unmountDisk /dev/disk${usbdrivenumber}
+    sudo dd if=${1}.dmg bs=1M of=/dev/rdisk${usbdrivenumber}
+}
+
