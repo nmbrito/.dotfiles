1469e3d68937d52de327513b51525b717d5a768e
nmbrito <brito.n@outlook.pt>
2025-06-19 17:54:24 +0100

    Updated INSTALL/utilities/defineFlatpak.sh.

---
+++ INSTALL/utilities/defineFlatpak.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+flatpakManager="flatpak"
+flatpakUpdate="flatpak update"
+flatpakInstallCommand="flatpak install"
+flatpakInstallCommandAuto="flatpak install -y"
+
+$flat_signaldesktop="org.signal.Signal"
+$flat_remotedesktop="net.devolutions.RDM"
+$flat_obsidianmd="md.obsidian.Obsidian"
+$flat_puddletag="net.puddletag.puddletag"
+
