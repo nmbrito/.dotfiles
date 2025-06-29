f1c0e27e7262d59169d45481d972015b5b4fdc17
nmbrito <brito.n@outlook.pt>
2025-06-29 10:25:17 +0100

    Updated INSTALL/setup.sh.

--- INSTALL/setup.sh
+++ INSTALL/setup.sh
@@ -1,50 +1,20 @@
 #!/bin/sh
+# ShellCheck disable=SC2034 # Allow unused variables
 
-# Variables
-pathScript=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)                    # Run script from any directory
-pathUtilities=$(CDPATH= cd -- "$(dirname -- "$0")" && cd utilities && pwd) # Directory containing all utilities
-pathDotRoot=$(git rev-parse --show-toplevel)                               # Define .dotfiles directory
-pathCache="${HOME}/.cache"                                                 # Define .cache directory
-
-pathKDEThemes="${HOME}/.local/share"
-pathMacOSAppSupport="${HOME}/Library/Application Support"
-pathMacOSLibPreference="${HOME}/Library/Preferences"
-
-pathSysDevDMI="/sys/devices/virtual/dmi"
-pathiSH="/proc/ish"
-pathSWVers="/usr/bin/software_vers"
-
-catSysDevBoardVendor=$(cat /sys/devices/virtual/dmi/id/board_vendor) 2>&1
-catSysDevProdVendor=$(cat /sys/devices/virtual/dmi/id/product_version)
-catSysDevProdName=$(cat /sys/devices/virtual/dmi/id/product_name)
-
-runningDesktopEnvironment="${XDG_SESSION_DESKTOP}"
-zshSharePath="/usr/share/zsh"
-zshBinPath="/usr/bin/zsh"
-
-urlNerdFonts="https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest"
-urlOhMyPosh="https://ohmyposh.dev/install.sh"
-
-messageLongWarn="###########################################"
-messageLongDash="----------------------"
-messageExecRoot="# The following commands will run as ROOT #"
-
-if   [ -n ${WT_SESSION} ];                  then wslSession=1; else wslSession=0; fi  # WSL session
-if   [ -L /etc/os-release ];                then . /etc/os-release                    # Linux distribution
-elif [ $(command -v sw_vers) 2>/dev/null ]; then ID="$(sw_vers -productName)"; fi     # macOS
-
-if [ ! -f ${pathUtilities}/functions.sh ]                 && \
-    [ ! -f ${pathUtilities}/defineAlmaLinux.sh ]          && \
-    [ ! -f ${pathUtilities}/defineAlpine.sh ]             && \
-    [ ! -f ${pathUtilities}/defineArchLinux.sh ]          && \
-    [ ! -f ${pathUtilities}/defineDebian.sh ]             && \
-    [ ! -f ${pathUtilities}/defineFlatpak.sh ]            && \
-    [ ! -f ${pathUtilities}/defineMacOS.sh ]              && \
-    [ ! -f ${pathUtilities}/defineOpenSUSETumbleweed.sh ] && \
-    [ ! -f ${pathUtilities}/packagesLists.sh ];           then
+if [ ! -f ${pathUtilities}/functions.sh ]                    \
+    && [ ! -f ${pathUtilities}/variables.sh ]                \
+    && [ ! -f ${pathUtilities}/defineAlmaLinux.sh ]          \
+    && [ ! -f ${pathUtilities}/defineAlpine.sh ]             \
+    && [ ! -f ${pathUtilities}/defineArchLinux.sh ]          \
+    && [ ! -f ${pathUtilities}/defineDebian.sh ]             \
+    && [ ! -f ${pathUtilities}/defineFlatpak.sh ]            \
+    && [ ! -f ${pathUtilities}/defineMacOS.sh ]              \
+    && [ ! -f ${pathUtilities}/defineOpenSUSETumbleweed.sh ] \
+    && [ ! -f ${pathUtilities}/packagesLists.sh ]; then
     printf '%s\n' "Missing components. Aborting."
     exit 0
 else
+    . ${pathUtilities}/variables.sh     # Source file containing functions.
     . ${pathUtilities}/functions.sh     # Source file containing functions.
     functionSystemDefineDistro          # Defines the package manager and software especific to the running distribution.
     functionSystemDefineHost            # Define current host
@@ -52,42 +22,60 @@ else
 fi
 
 # Main
-printf '%s\n' ""                  \
-              "Starting process"  \
-              "${messageLongDash}"\
-              ""                  \
+printf '%s\n' ""                   \
+              "Starting process"   \
+              "${messageLongDash}"
 
-while :
-do
+while : ; do
     functionSystemBuildMenu
     printf '%s' "Option: "
-    read -r optionPicked
+    read -r option_Selected
 
     printf '%s\n' ""
 
-    case "${optionPicked}" in
-        1) # Run All
-            functionImplementRepositories
-            functionInstallPackages
-            functionRollFixes
-            functionGetFonts
-            functionPerformSymlinks
-            functionConfigureGitGlobals
-            functionSyncGitSubmodules
-            functionRestoreKDEThemes
-            functionRollZSHShell
-            functionRebuilGitSubmodules
+    case "${option_Selected}" in
+        1) 
+            function_RollRepositories
+            function_RollFixes
+            function_RollPackages
+            function_RollFonts
+            function_RollSymlinks
+            function_RollZSHShell
+            function_ConfigureGitGlobals
+            function_SyncGitSubmodules
+            function_RestoreExtraConfigs
+            function_RebuilGitSubmodules
             ;;
-        2)  functionImplementRepositories;; # Repositories
-        3)  functionInstallPackages      ;; # Packages
-        4)  functionRollFixes            ;; # Fixes
-        5)  functionGetFonts             ;; # Fonts
-        6)  functionPerformSymlinks      ;; # Symlinks
-        7)  functionConfigureGitGlobals  ;; # Git Globals
-        8)  functionSyncGitSubmodules    ;; # Git Submodules
-        9)  functionRollZSHShell         ;; # ZSH Shell
-        10) functionRestoreKDEThemes     ;; # Restore KDE Themes
-        r)  functionRebuilGitSubmodules  ;; # Rebuild Git Submodules
+        2)
+            function_RollRepositories
+            ;;
+        3)
+            function_RollFixes
+            ;;
+        4)
+            function_RollPackages
+            ;;
+        5)
+            function_RollFonts
+            ;;
+        6)
+            function_RollSymlinks
+            ;;
+        7)
+            function_RollZSHShell
+            ;;
+        8)
+            function_SyncGitSubmodules
+            ;;
+        9)
+            function_ConfigureGitGlobals
+            ;;
+        10)
+            function_RestoreExtraconfigs
+            ;;
+        r)
+            function_RebuilGitSubmodules
+            ;;
         *)
             printf '%s\n' "Exiting..." \
                           ""
