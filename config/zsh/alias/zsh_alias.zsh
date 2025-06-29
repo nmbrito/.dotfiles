f94603554fd124b372be02d41b7084329224c9d0
nmbrito <brito.n@outlook.pt>
2025-06-29 10:25:17 +0100

    Updated config/zsh/alias/zsh_alias.zsh.

--- config/zsh/alias/zsh_alias.zsh
+++ config/zsh/alias/zsh_alias.zsh
@@ -5,51 +5,50 @@ alias lst="eza --header --long --tree --level=2 --all --group --sort=type --icon
 alias sl="'ls' -slaF --color=auto"                                                              # Use LS to list directory
 alias slh="'ls' -sl --color=auto"                                                               # Use LS to list directory minus hidden
 
-alias cat="bat"                                                                     # Subs cat for bat
-alias reload=". ${HOME}/.zshrc"                                                     # reloads zshrc config
-alias rm.ds="find . -name .DS_Store -print0 | xargs -0 git rm -f --ignore-unmatch"  # Removes .DS_Store in git repository
-alias vis="vim -S ${nmb_vimsessions}/current_session.vim"                           # Loads vim current session
+alias cat="bat"                                                                                 # Subs cat for bat
+alias reload=". ${HOME}/.zshrc"                                                                 # reloads zshrc config
+alias rm.ds="find . -name .DS_Store -print0 | xargs -0 git rm -f --ignore-unmatch"              # Removes .DS_Store in git repository
+alias vis="vim -S ${nmb_vimsessions}/current_session.vim"                                       # Loads vim current session
 
-alias cd.="cd ${nmb_dotrepo}"   # Changes to .dotfiles directory
-alias ..="cd .."                # Changes down one directory
-alias ...="cd ../.."            # Changes down two directories
-alias ....="cd ../../.."        # Changes down three directories
-alias .....="cd ../../../.."    # Changes down four directories
+alias cd.="cd ${nmb_dotrepo}"                                                                   # Changes to .dotfiles directory
+alias ..="cd .."                                                                                # Changes down one directory
+alias ...="cd ../.."                                                                            # Changes down two directories
+alias ....="cd ../../.."                                                                        # Changes down three directories
+alias .....="cd ../../../.."                                                                    # Changes down four directories
 
-alias susebox="distrobox enter susebox"     # Enter susebox container
-alias debianbox="distrobox enter debianbox" # Enter debianbox container
+alias susebox="distrobox enter susebox"                                                         # Enter susebox container
+alias debianbox="distrobox enter debianbox"                                                     # Enter debianbox container
 
-alias ompup="oh-my-posh upgrade"    # Upgrade ohmyposh
+alias ompup="oh-my-posh upgrade"                                                                # Upgrade ohmyposh
 
-alias gitsubup="git submodule update --remote"              # Updates submodules
-alias gitsubinit="git submodule update --init --recursive"  # Initiates all submodules
-alias gitk="git status"                                     # Informs the status of the repository
-alias gitf="git fetch"                                      # Verifies if remote branch is newer
+alias gitsubup="git submodule update --remote"                                                  # Updates submodules
+alias gitsubinit="git submodule update --init --recursive"                                      # Initiates all submodules
+alias gitk="git status"                                                                         # Informs the status of the repository
+alias gitf="git fetch"                                                                          # Verifies if remote branch is newer
 
 # Function alias
-function shcheck()                          # Shellcheck with color and less active
+function shcheck()
 {
+    # Shellcheck with color and less active
     shellcheck "$1" -x --color | less -r
 }
-
-function gitunicom()    # Add and comment individual files
+function gitunicom()
 {
-    for file in $(git status --porcelain | cut -b 4-)
-    do
-        echo "${file}"
+    # Add and comment individual files
+    for file in $(git status --porcelain | cut -b 4-); do
+        printf '%s\n' "$file"
         git add "$(git rev-parse --show-toplevel)/${file}"
         printf '%s' "Enter comment: "
         read -r comments
-        git commit -m "${comments}"
+        git commit -m "$comments"
     done
     git push
 }
-
-function gitquickcom()  # Add and comment all files
+function gitquickcom()
 {
-    for file in $(git status --porcelain | cut -b 4-)
-    do
-        echo "${file}"
+    # Add and comment all files
+    for file in $(git status --porcelain | cut -b 4-); do
+        printf '%s\n' "$file"
         git add "$(git rev-parse --show-toplevel)/${file}"
         git commit -m "Updated ${file}."
     done
