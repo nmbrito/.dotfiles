018d4e3a4caff18cc52d769e6a7a6c58d28b7963
nmbrito <brito.n@outlook.pt>
2025-07-29 19:03:35 +0100

    Updated INSTALL/utilities/define_macOS.sh.

---
+++ INSTALL/utilities/define_macOS.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Commands
+distroName="macOS"
+
+packageManager="brew"
+
+packageInstall="brew bundle install"
+packageInstallAuto="brew bundle install"
+packageInstallPattern=""
+
+systemUpdate="brew update"
+systemUpgrade="brew upgrade"
+systemUpgradeAuto="brew upgrade"
+systemClean="" 
+systemCleanAuto="" 
+systemRemove="" 
+systemRemoveAuto="" 
+
+repoAdd=""
+repoRefresh=""
+repoImport=""
+repoAutoGPGKeys=""
+
+# Repositories
+
+# Packages
