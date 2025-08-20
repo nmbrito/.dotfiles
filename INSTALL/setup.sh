eb44bcc6ce23660df58f973247a8f206a6cd4702
nmbrito <brito.n@outlook.pt>
2025-08-20 21:00:37 +0100

    Updated INSTALL/setup.sh.

--- INSTALL/setup.sh
+++ INSTALL/setup.sh
@@ -19,7 +19,6 @@ fi
 printf '%s\n' ""                   \
               "Starting process"   \
               "${message_LongDash}"
-echo $wsl_Session
 
 printf '%s\n' "${c_Bold}${fg_Red}This script will run certain sections with elevated privileges${c_Normal}"
 function_SystemAskForSudoPassword
