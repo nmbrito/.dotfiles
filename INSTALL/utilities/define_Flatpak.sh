7d5fcf90ba1b857c8b4d52baa43cbf083fe48723
nmbrito <brito.n@outlook.pt>
2025-12-25 19:28:18 +0000

    Updated INSTALL/utilities/define_Flatpak.sh.

--- INSTALL/utilities/define_Flatpak.sh
+++ INSTALL/utilities/define_Flatpak.sh
@@ -6,7 +6,7 @@ flatpak_Install="flatpak install"
 flatpak_InstallAuto="flatpak install -y"
 
 flat_signaldesktop="org.signal.Signal"
-flat_remotedesktop="net.devolutions.RDM"
+flat_remotedesktop="com.devolutions.remotedesktopmanager"
 flat_obsidianmd="md.obsidian.Obsidian"
 flat_puddletag="net.puddletag.puddletag"
-
+flat_waterfox="net.waterfox.waterfox"
