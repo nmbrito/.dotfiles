6fca2aeba358ddf557c486f850f3226ac9985326
nmbrito <brito.n@outlook.pt>
2026-02-08 17:11:19 +0000

    Updated config/zsh/alias/zsh_alias.zsh.

--- config/zsh/alias/zsh_alias.zsh
+++ config/zsh/alias/zsh_alias.zsh
@@ -28,24 +28,11 @@ alias gitsubinit="git submodule update --init --recursive"                      
 alias gitk="git status"                                                                         # Informs the status of the repository
 alias gitf="git fetch"                                                                          # Verifies if remote branch is newer
 
-# Function alias
-function shcheck()
-{
-    # Shellcheck with color and less active
-    shellcheck "$1" -x --color | less -r
-}
-function gitunicom()
-{
-    # Add and comment individual files
-    for file in $(git status --porcelain | cut -b 4-); do
-        printf '%s\n' "$file"
-        git add "$(git rev-parse --show-toplevel)/${file}"
-        printf '%s' "Enter comment: "
-        read -r comments
-        git commit -m "$comments"
-    done
-    git push
-}
+alias shcheck="shellcheck $1 -x --color | less -r"
+alias gitunicom="sh ${nmb_zshconfig}/alias/zsh_function_GitUniqueCommentsCommit.sh"
+#alias gitquickcom="sh ${nmb_zshconfig}/alias/zsh_function_GitQuickCommit.sh"
+alias backup_plasma="sh ${nmb_zshconfig}/alias/zsh_function_BackupKDE.sh"
+
 function gitquickcom()
 {
     # Add and comment all files
@@ -55,37 +42,4 @@ function gitquickcom()
         git commit -m "Updated ${file}."
     done
     git push
-}
-function backup_plasma()
-{
-    $date_today=$(date +%F)
-    $plasma_listconfig="kdeglobals \
-    konsolerc \
-    ksmserverrc \
-    ksplashrc \
-    ktimezonedrc \
-    kwinrc \
-    plasma-localerc \
-    plasma-org.kde.plasma.desktop-appletsrc \
-    plasmaparc \
-    plasmarc \
-    plasmashellrc \
-    powerdevilrc \
-    "
-    $plasma_listshare="konsole/ \
-    kwin/effects/ \
-    plasma/look-and-feel/ \
-    plasma/plasmoids/ \
-    plasma/wallpapers/ \
-    "
-
-    mkdir -p ${nmb_dotrepo}/config/kde_plasma/backup_${date_today}/home_config/
-    mkdir -p ${nmb_dotrepo}/config/kde_plasma/backup_${date_today}/local_share/
-
-    for eachFile in $plasma_listconfig; do
-        cp -rv "${HOME}/.config/$eachFile" "${nmb_dotrepo}/config/kde_plasma/backup_${date_today}/home_config/"
-    done
-    for eachFolder in $plasma_listshare; do
-        cp -rv "${HOME}/.local/share/${eachFolder}" "${nmb_dotrepo}/config/kde_plasma/backup_${date_today}/local_share/"
-    done
 }
